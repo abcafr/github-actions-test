3b8cbbc9b9a1041dad881c9305eb969a86f8d152
abcafr <abcafr@almbrand.dk>
2021-10-27 13:44:14 +0200

    api is close to working, added payload print to actions.yml

--- api.py
+++ api.py
@@ -1,15 +1,20 @@
 # This API is used to learn the 'repository_dispatch' event trigger in a workflow.
 # Use it to play around and make requests to your GitHub repository to
 import requests
+import json
 
 headers = {
     "Accept": "application/vnd.github.everest-preview+json",
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Authorization": "token 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"
 }
 
 data = {
     "event_type": "build"
-
 }
+
 r = requests.post(
-    'https://github.com/abcafr/github-actions-test/actions/dispatches', data=data, headers=headers)
+    'https://api.github.com/repos/abcafr/github-actions-test/dispatches', data=data, headers=headers)
+
+print(r.status_code)
+print(json.dumps(r.json()))
